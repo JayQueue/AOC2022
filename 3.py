1e850f07ad49eb1e1690a261b14ed7c10347ccb9
JayQueue <jonathan.quartier@gmail.com>
2023-10-24 07:59:03 +0200

    Added part 2

--- 3.py
+++ 3.py
@@ -1,15 +1,14 @@
 # Part 1
 som = 0
 with open("3_input_full.txt") as f:
-     for line in f:
-         middle = len(line)//2
-         for letter in set(line.strip()[0:middle]).intersection(line.strip()[middle:]):
-             if letter.islower():
-                 som += ord(letter) - 96
-             else:
-                 som += (ord(letter) - 64) + 26
-     # Part 1
-     print(f"Sum part 1: {som}")
+    for line in f:
+        middle = len(line)//2
+        for letter in set(line.strip()[0:middle]).intersection(line.strip()[middle:]):
+            if letter.islower():
+                som += ord(letter) - 96
+            else:
+                som += (ord(letter) - 64) + 26
+    print(f"Sum part 1: {som}")
 
 
 # Part 2
@@ -17,7 +16,7 @@ som2 = 0
 with open("3_input_full.txt") as f2:
     line2 = f2.readlines()
     for i in range(0, len(line2), 3):
-        listper3 = [set(p2line.strip('\n')) for p2line in line2[i:i + 3]]
+        listper3 = [set(f2line.strip('\n')) for f2line in line2[i:i + 3]]
         for letter in set.intersection(*listper3):
             if letter.islower():
                 som2 += ord(letter) - 96
